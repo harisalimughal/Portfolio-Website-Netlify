5b998c8884e7ed1a5578fba2bad7eb4959529103
Haris <119020619+harisalimughal@users.noreply.github.com>
2023-07-20 23:55:24 +0500

    pages/index.jsutils/global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Muhammad Haris Ali Mughal';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'I am a web developer. I can put your imagination into technology. I will be your creative web development partner';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
